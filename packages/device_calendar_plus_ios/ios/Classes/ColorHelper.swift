7c03be75c90dcf661ce77155918547401f3bc0c5
kevsjh <kevsjh@gmail.com>
2026-01-01 23:18:08 +0800

    fix: typo

--- packages/device_calendar_plus_ios/ios/Classes/ColorHelper.swift
+++ packages/device_calendar_plus_ios/ios/Classes/ColorHelper.swift
@@ -22,7 +22,7 @@ class ColorHelper {
     }
     
     let r = Int(components[0] * 255.0)
-    let g = Int(components[1] * 255.0)frfr
+    let g = Int(components[1] * 255.0)
     let b = Int(components[2] * 255.0)
     
     return String(format: "#%02X%02X%02X", r, g, b)
